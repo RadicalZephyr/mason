59b9c9125ed581e8643d53a3cf0b38576a6cdc9d
Geoff Shannon <geoffpshannon@gmail.com>
2019-02-03 15:42:47 -0800

    Get a basic version working

--- src/builder.rs
+++ src/builder.rs
@@ -2,14 +2,97 @@ use proc_macro2::{Ident, Span, TokenStream};
 
 use quote::quote;
 
-pub fn expand_derive_builder(item: &syn::DeriveInput) -> Result<TokenStream, Vec<syn::Error>> {
+use syn::{Data, DataStruct, DeriveInput, Field, Fields};
+
+pub fn expand_derive_builder(item: &DeriveInput) -> Result<TokenStream, Vec<syn::Error>> {
+    let vis = &item.vis;
     let ident = &item.ident;
+    let generics = &item.generics;
+    let data = &item.data;
+
     let builder_type_ident = Ident::new(&format!("{}Builder", ident), Span::call_site());
+
+    let fields = get_fields(&data);
+
+    let builder_fields = make_builder_fields(&fields);
+    let builder_fns = make_builder_fns(&fields);
+    let constructor_fields = make_constructor_fields(&fields);
+
     Ok(quote! {
-        pub struct #builder_type_ident;
+        #[derive(Default)]
+        #vis struct #builder_type_ident #generics {
+            #( #builder_fields ),*
+        }
+
+        impl #builder_type_ident {
+            #( #builder_fns )*
+
+            #vis fn build(self) -> #ident {
+                #ident {
+                    #( #constructor_fields ),*
+                }
+            }
+        }
 
         impl #ident {
-            fn builder() -> #builder_type_ident { #builder_type_ident }
+            #vis fn builder() -> #builder_type_ident {
+                #builder_type_ident::default()
+            }
         }
     })
+}
+
+fn get_fields(data: &Data) -> Vec<&Field> {
+    match data {
+        Data::Struct(data) => get_struct_fields(data),
+        _ => vec![],
+    }
+}
+
+fn get_struct_fields(data: &DataStruct) -> Vec<&Field> {
+    match &data.fields {
+        Fields::Named(named_fields) => named_fields.named.iter().collect(),
+        _ => vec![],
+    }
+}
+
+fn make_builder_fields<'a>(fields: &'a Vec<&'a Field>) -> Vec<TokenStream> {
+    fields
+        .iter()
+        .map(|field| {
+            let ident = &field.ident;
+            let ty = &field.ty;
+            quote! {
+                        #ident: Option<#ty>,
+            }
+        })
+        .collect()
+}
+
+fn make_builder_fns<'a>(fields: &'a Vec<&'a Field>) -> Vec<TokenStream> {
+    fields
+        .iter()
+        .map(|field| {
+            let ident = &field.ident;
+            let ty = &field.ty;
+            quote! {
+                fn #ident(mut self, #ident: #ty) -> Self {
+                    self.#ident = Some(#ident);
+                    self
+                }
+            }
+        })
+        .collect()
+}
+
+fn make_constructor_fields<'a>(fields: &'a Vec<&'a Field>) -> Vec<TokenStream> {
+    fields
+        .iter()
+        .map(|field| {
+            let ident = &field.ident;
+            quote! {
+                #ident: self.#ident.unwrap_or_else(Default::default)
+            }
+        })
+        .collect()
 }
--- tests/smoke.rs
+++ tests/smoke.rs
@@ -1,11 +1,12 @@
 use mason::Builder;
 
-#[derive(Builder)]
+#[derive(Builder, Clone, Debug, PartialEq)]
 pub struct Number {
     num: u8,
 }
 
 #[test]
 fn works() {
-    let num_builder = Number::builder();
+    let actual = Number::builder().num(12).build();
+    assert_eq!(Number { num: 12 }, actual);
 }
